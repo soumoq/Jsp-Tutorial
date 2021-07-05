3feb2639d0669c165cdbea7f50a6248d9f8e21c2
soumoq <soumojyotiganguly270@gmail.com>
2021-07-05 17:44:14 +0530

    Deferance between do get and do post

--- src/main/java/com/mycompany/jspprogram/AddServlet.java
+++ src/main/java/com/mycompany/jspprogram/AddServlet.java
@@ -1,12 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.jspprogram;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 public class AddServlet extends HttpServlet {
 
     @Override
-    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
         int i = Integer.parseInt(req.getParameter("description1"));
         int j = Integer.parseInt(req.getParameter("description2"));
 
@@ -25,8 +22,7 @@ public class AddServlet extends HttpServlet {
 
     }
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-    }
+
+    
+    
 }
